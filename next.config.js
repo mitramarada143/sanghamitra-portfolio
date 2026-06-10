dd22321f10c4da1baa843d8c66c3286d5a7cbd53
mitramarada143 <mitra.marada143@gmail.com>
2026-06-10 11:29:34 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  headers: async () => [
+    {
+      source: "/:path*",
+      headers: [
+        {
+          key: "X-DNS-Prefetch-Control",
+          value: "on"
+        },
+        {
+          key: "X-Frame-Options",
+          value: "SAMEORIGIN"
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff"
+        },
+        {
+          key: "X-XSS-Protection",
+          value: "1; mode=block"
+        },
+        {
+          key: "Referrer-Policy",
+          value: "strict-origin-when-cross-origin"
+        }
+      ]
+    }
+  ]
+};
+
+module.exports = nextConfig;
